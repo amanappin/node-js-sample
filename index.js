8be5b6c9aae3876b9c312564dc45e0a00119080d
amanappin <106663187+amanappin@users.noreply.github.com>
2022-08-02 22:19:56 +0530

    Update index.js
    mbm

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello AMAN is Here! JS Project Hello How are you ')
+  response.send('Hello AMAN is Here! this is Node.JS Project. Hello How are you ')
 }) 
 
 app.listen(app.get('port'), function() {

ef7bfbb0aca45008d64e14d4637627fa9f7f5674
Vishnu Kushwaha <77265304+V1SHNU08@users.noreply.github.com>
2022-08-02 14:12:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('APPINVENTIV TEST ')
+  response.send('APPINVENTIV TESTING ')
 }) 
 
 app.listen(app.get('port'), function() {
